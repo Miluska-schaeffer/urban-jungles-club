a7008d93d31bd605b531f89f3e6937719ce6c00f
miluska schaeffer <miluska.schaeffer@gmail.com>
2021-01-20 12:37:27 +0100

    jungles index cards fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @jungles = Jungle.order(created_at: :desc)
+    @jungles = Jungle.order(created_at: :desc).limit(7)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -49,6 +49,32 @@ Jungle.create(
   banner_url: 'https://images.unsplash.com/photo-1591870510340-c8578d6c3b5f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2734&q=80'
 )
 
+Jungle.create(
+  name: 'Jungle of Paul',
+  banner_url: 'https://images.unsplash.com/photo-1591870510340-c8578d6c3b5f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2734&q=80'
+)
+
+
+Jungle.create(
+  name: 'Chicha\'s Jungle',
+  banner_url: 'https://images.unsplash.com/photo-1604762525827-61f629d82f1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+)
+
+Jungle.create(
+  name: 'Frank\'s Secret Garden',
+  banner_url: 'https://images.unsplash.com/photo-1552758070-02c40ecffd5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1526&q=80'
+)
+
+Jungle.create(
+  name: 'Carla\'s Urban Oasis',
+  banner_url: 'https://images.unsplash.com/photo-1544860707-c352cc5a92e3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1915&q=80'
+)
+
+
+Jungle.create(
+  name: 'Joe\'s Jungle',
+  banner_url: 'https://images.unsplash.com/photo-1603436326363-e4693ce2757c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80'
+)
 
 
 puts "#{Jungle.count} jungles created."
